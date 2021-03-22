054740f9066af936f88bb425197eef6c7a4b324d
choijinhyun <chjh0902@naver.com>
2021-03-22 20:01:18 +0900

    K번째수

---
+++ src/Level1/Solution6.java
@@ -0,0 +1,17 @@
+package Level1;
+
+import java.util.*;
+
+class Solution6 {
+    public int[] solution(int[] array, int[][] commands) {
+        int len = commands.length;
+        int[] answer = new int[len];
+
+        for(int i=0; i<len; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
